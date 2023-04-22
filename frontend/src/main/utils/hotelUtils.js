639a1c2a7f082a5a7569c424ea103fa299e1945f
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2023-04-22 16:16:49 -0700

    by - Add stubs and tests for hotelUtils

---
+++ frontend/src/main/utils/hotelUtils.js
@@ -0,0 +1,38 @@
+// get restaurants from local storage
+const get = () => {
+  // details omitted
+};
+
+const getById = (id) => {
+  // details omitted
+}
+
+// set restaurants in local storage
+const set = (restaurantCollection) => {
+  // details omitted
+};
+
+// add a restaurant to local storage
+const add = (restaurant) => {
+  // details omitted
+};
+
+// update a restaurant in local storage
+const update = (restaurant) => {
+  // details omitted
+};
+
+// delete a restaurant from local storage
+const del = (id) => {
+  // details omitted
+};
+
+const hotelUtils = {
+  get,
+  getById,
+  add,
+  update,
+  del
+};
+
+export { hotelUtils };
